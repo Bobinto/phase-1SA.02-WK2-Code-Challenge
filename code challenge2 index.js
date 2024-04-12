1905f2e0d2f1eec11490ec70a5c8649d798359f0
Bobinto <162312240+Bobinto@users.noreply.github.com>
2024-04-12 11:57:45 +0300

    Update code challenge2 index.js

--- code challenge2 index.js
+++ code challenge2 index.js
@@ -11,7 +11,5 @@ function generateArray(start, end) {
     }
     return result;
 }
-
-// Test the function
-console.log(generateArray(4, 7)); // Output: [4, 5, 6, 7]
-console.log(generateArray(-4, 7)); // Output: [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+console.log(generateArray(4, 7));
+console.log(generateArray(-4, 7)); 
