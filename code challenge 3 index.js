a41038c6079cef34dafee61df852d5bd48cca35a
Bobinto <162312240+Bobinto@users.noreply.github.com>
2024-04-17 11:38:56 +0300

    Update code challenge 3 index.js

--- code challenge 3 index.js
+++ code challenge 3 index.js
@@ -1,25 +1,18 @@
 function isPrime(num) {
-    if (num <= 1) {
-        return false;
-    }
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
     for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
-        }
+        if (num % i === 0) return false;
     }
+
     return true;
 }
 
-function findPrimes(numbers) {
-    const primeNumbers = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (isPrime(numbers[i])) {
-            primeNumbers.push(numbers[i]);
-        }
-    }
-    return primeNumbers;
+function filterPrimeNumbers(inputArray) {
+    return inputArray.filter(num => isPrime(num));
 }
 
-// Test the function
 const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(findPrimes(inputArray)); // Output: [2, 3, 5, 7]
+const outputArray = filterPrimeNumbers(inputArray);
+console.log(outputArray); 
