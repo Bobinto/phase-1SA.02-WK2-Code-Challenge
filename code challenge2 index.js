a1c3e42bfc6936035e16cd57a793460318308d59
Bobinto <162312240+Bobinto@users.noreply.github.com>
2024-04-17 11:37:24 +0300

    Update code challenge2 index.js

--- code challenge2 index.js
+++ code challenge2 index.js
@@ -1,15 +1,17 @@
-function generateArray(start, end) {
-    let result = [];
-    if (start <= end) {
-        for (let i = start; i <= end; i++) {
-            result.push(i);
-        }
-    } else {
-        for (let i = start; i >= end; i--) {
-            result.push(i);
-        }
+function generateNumberArray(num1, num2) {
+    const result = [];
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+
+    for (let i = start; i <= end; i++) {
+        result.push(i);
     }
+
     return result;
 }
-console.log(generateArray(4, 7));
-console.log(generateArray(-4, 7)); 
+
+const outputArray1 = generateNumberArray(4, 7);
+console.log(outputArray1);
+
+const outputArray2 = generateNumberArray(-4, 7);
+console.log(outputArray2);
